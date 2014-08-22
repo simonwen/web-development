f804ea9485cfc1232185c718fecd5101f45b2ccc
simonwen <wen-lu.lv@hp.com>
2014-08-22 10:13:02 +0800

    Update init.pp

--- puppet/modules/tomcat_config/manifests/init.pp
+++ puppet/modules/tomcat_config/manifests/init.pp
@@ -1,5 +1,5 @@
-class tomcat_config {
-    package {'tomcat':
-        ensure => present,
-    }
+class tomcat_config { 'tomcat_config':
+  version     => 6,
+  sources     => true,
+  sources_src => 'http://archive.apache.org/dist/tomcat/',
 }
